875d0ef0f12a8bfddf2615f46bea010c4ed2c118
Darnion <Darnion@DESKTOP-PIQMCKV>
2023-12-21 22:35:49 +0300

    LoyaltyCardEntityTypeConfiguration

---
+++ AutoRest.Context.Configuration/LoyaltyCardEntityTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using AutoRest.Context.Contracts.Models;
+
+namespace AutoRest.Context.Configuration
+{
+    public class LoyaltyCardEntityTypeConfiguration : IEntityTypeConfiguration<LoyaltyCard>
+    {
+        public void Configure(EntityTypeBuilder<LoyaltyCard> builder)
+        {
+            builder.ToTable("LoyaltyCards");
+            builder.HasIdAsKey();
+            builder.PropertyAuditConfiguration();
+            builder.Property(x => x.Number).IsRequired();
+
+            builder
+               .HasMany(x => x.OrderItem)
+               .WithOne(x => x.LoyaltyCard)
+               .HasForeignKey(x => x.LoyaltyCardId);
+
+            builder.HasIndex(x => x.LoyaltyCardType)
+                .HasDatabaseName($"IX_{nameof(LoyaltyCard)}_{nameof(LoyaltyCard.LoyaltyCardType)}");
+        }
+    }
+}

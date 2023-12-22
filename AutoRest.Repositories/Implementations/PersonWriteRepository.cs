189a076619bbdd45998648749f4ee4d46be5c90e
Darnion <Darnion@DESKTOP-PIQMCKV>
2023-12-22 13:51:05 +0300

    Реализация чтения и записи репозиториев

---
+++ AutoRest.Repositories/Implementations/PersonWriteRepository.cs
@@ -0,0 +1,20 @@
+﻿using AutoRest.Common.Entity.InterfaceDB;
+using AutoRest.Context.Contracts.Models;
+using AutoRest.Repositories.Contracts;
+
+namespace AutoRest.Repositories.Implementations
+{
+    /// <inheritdoc cref="IPersonWriteRepository"/>
+    public class PersonWriteRepository : BaseWriteRepository<Person>,
+        IPersonWriteRepository,
+        IRepositoryAnchor
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="PersonWriteRepository"/>
+        /// </summary>
+        public PersonWriteRepository(IDbWriterContext writerContext)
+            : base(writerContext)
+        {
+        }
+    }
+}
